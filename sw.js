7e2d29ca4b7d1edc68ed01f05b649367e72bbc0f
itsRide Limo Toronto <fazal@itsride.org>
2026-08-26 05:52:54 -0400

    Refresh quote calculator cache for V1.6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='itsride-quote-v1-5';
+const CACHE='itsride-quote-v1-6';
 const ASSETS=['./','./index.html','./config.js','./logo.png','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
